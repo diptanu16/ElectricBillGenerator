26af51a7f31cfe3761d773d6d22e5af2c3816bb5
Diptanu <diptanudnath@gmail.com>
2021-11-17 13:15:34 +0530

    Update Data

---
+++ src/App.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        String Name;
+        String Address;
+        char CustomerType;
+        double previous;
+        double present;
+        double consume;
+        double amount;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Account Name: ");
+        Name = sc.nextLine();
+        System.out.println("Enter Address: ");
+        Address = sc.nextLine();
+        System.out.println("Account name is: " + Name);
+        System.out.println("Address is : " + Address);
+        System.out.println("Choose bill type B for(Business) or R for (Residential): ");
+        CustomerType = sc.next().charAt(0);
+        switch (CustomerType) {
+            case 'R':
+                System.out.println("Enter Previous Reading: ");
+                previous = sc.nextDouble();
+                System.out.println("Enter Present Reading:");
+                present = sc.nextDouble();
+                consume = present - previous;
+                System.out.println("Current consumed in kilowatt(Residential): " + consume);
+                System.out.println("----------------------------------------");
+                if (consume <= 50) {
+                    amount = consume * 4.17;
+                    System.out.println("Bill amount is Rs." + amount);
+                } else if (consume >= 51 && consume <= 150) {
+                    amount = consume * 5.46;
+                    System.out.println("Bill amount is Rs." + amount);
+                } else if (consume >= 152 && consume <= 300) {
+                    amount = consume * 6.16;
+                    System.out.println("Bill amount is Rs." + amount);
+                } else {
+                    amount = consume * 7.20;
+                    System.out.println("Bill amount is Rs." + amount);
+
+
+                }
+            case 'B':
+                System.out.println("Enter Previous Reading: ");
+                previous = sc.nextInt();
+                System.out.println("Enter Present Reading:");
+                present = sc.nextInt();
+                consume = present - previous;
+                System.out.println("Current consumed in kilowatt(Business): " + consume);
+                System.out.println("----------------------------------------");
+                if (consume <= 50) {
+                    amount = consume * 5.31;
+                    System.out.println("Bill amount is Rs." + amount);
+                } else if (consume >= 51 && consume <= 150) {
+                    amount = consume * 6.18;
+                    System.out.println("Bill amount is Rs." + amount);
+                } else {
+                    amount = consume * 7.25;
+                    System.out.println("Bill amount is Rs." + amount);
+
+                }
+                break;
+        }
+    }
+}
